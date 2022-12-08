52111dff126828b3acd141cee95543b81347fb42
Sakkarin Karoonwong <59549787+SakkarinK@users.noreply.github.com>
2022-12-08 15:38:20 +0700

    Add files via upload

---
+++ get_distances_between_species.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# Constructing DataFrame to use as an input for get_distances_between_species function.
+def construct_DataFrame(not_novel,
+                        not_novel_species, 
+                        novel, 
+                        novel_species):
+    not_novel = pd.DataFrame(not_novel)
+    not_novel['species'] = not_novel_species
+    not_novel_species = np.unique(not_novel_species)
+    
+    novel = pd.DataFrame(novel)
+    novel['species'] = novel_species
+    novel_species = np.unique(novel_species)
+        
+    return not_novel, not_novel_species, novel, novel_species
+
+
+class Distances:
+    def get_distances_between_species(self,
+                                      not_novel,
+                                      not_novel_species,
+                                      novel, 
+                                      novel_species,
+                                      distance):
+        self.not_novel = not_novel
+        self.not_novel_species = not_novel_species
+        self.novel = novel
+        self.novel_species = novel_species
+        self.distance = distance
+        
+        d = [] # contain mean of distances between not novel species i and novel species j
+        ns = [] # contain novel species label
+        nns = [] # contain not novel species
+        
+        not_novel, not_novel_species, novel, novel_species = construct_DataFrame(not_novel, 
+                                                                                 not_novel_species,
+                                                                                 novel, 
+                                                                                 novel_species)
+    
+        for i in not_novel_species:
+            for j in novel_species:
+                # calculate distance between not novel species i and novel species j
+                between_dist = pairwise_distances(novel.loc[novel.species == j, :].iloc[: , :-3],
+                                                  not_novel.loc[not_novel.species == i, :].iloc[: , :-3],
+                                                  metric = distance)
+                d.append(np.mean(between_dist))
+                nns.append(i)
+                ns.append(j)
+
+        # create dataframe of output
+        op_metric = pd.DataFrame({'Novel Species':ns,
+                                  'Not Novel Species':nns, 
+                                  'Mean of Distance':d})
+        return op_metric
